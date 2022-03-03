9af2659e9066877cbdc344cd7d49731e5399f9ca
Ola Amberntsson <ola.amberntsson@iths.se>
2022-03-03 09:21:51 +0100

    Added new_empoyee view and GetMapping for form. Not done yet with either

--- src/main/java/se/olaamberntsson/thymeleafwebapp/controller/EmployeeController.java
+++ src/main/java/se/olaamberntsson/thymeleafwebapp/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import se.olaamberntsson.thymeleafwebapp.model.Employee;
 import se.olaamberntsson.thymeleafwebapp.service.EmployeeService;
 
 
@@ -19,6 +20,20 @@ public class EmployeeController {
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+
+        //creates a model that binds to the forms data
+        Employee employee = new Employee();
+
+        //thymeleaf will access the empty employee object
+        model.addAttribute("employee", employee);
+
+        //thymeleaf template
+        return "new_employee";
+
     }
 
 }
